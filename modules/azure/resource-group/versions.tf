79bfcdcb689c2c5620a57ff876ee6b72f233dfe1
Simon Weald <glitchcrab-github@simonweald.com>
2020-08-03 15:58:43 +0100

    correct provider config

--- modules/azure/resource-group/versions.tf
+++ modules/azure/resource-group/versions.tf
@@ -2,9 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      # a features block must be defined, even if it is empty
-      features = {}
     }
   }
   required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  # a features block must be defined, even if it is empty
+  features {}
 }
